c4471a4c0f2fde4eeed2546d9db88c0997b73a11
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-02-16 19:07:46 -0300

    fix: fix layout import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '@/styles/Home.module.css'
-import Layout from '@/components/layout'
+import Layout from '@/components/Layout'
 import { useState } from 'react'
 import { getPokemon } from '@/services'
 import Card from '@/components/Card'
