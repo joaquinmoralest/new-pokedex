e7632ee22216b519f377872bc9e41cefabebbd25
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-03-09 18:56:05 -0300

    feat: Fetch all pokemons to render in home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
 import styles from '@/styles/Home.module.css'
 import Layout from '../components/Layout'
-import { useState } from 'react'
-import { getPokemon } from '@/services'
+import { useEffect, useState } from 'react'
+import { getAllPokemon, getPokemon } from '@/services'
 import Card from '../components/Card'
 import Input from '../components/Input'
 
 export default function Home() {
+  const [pokemonsList, setPokemonsList] = useState([])
   const [pokemon, setPokemon] = useState('')
   const [data, setData] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+
+  async function fetchData() {
+    const data = await getAllPokemon()
+    const {results} = data
+
+    const pokemonFullInfo = await Promise.all(results.map(async (pokemon) => {
+      const res = await getExtraInfo(pokemon.url)
+      return res
+    }))
+    setPokemonsList(pokemonFullInfo)
+  }
+
+  async function getExtraInfo(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    return data
+  }
   
   async function handleSubmit(e) {
     e.preventDefault()  
@@ -25,6 +44,12 @@ export default function Home() {
   function handleChange(e) {
     setPokemon(e.target.value)
   }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  // console.log(pokemonsList)
   
   return (
     <Layout>
@@ -43,6 +68,25 @@ export default function Home() {
           />
         </> 
       )}
+
+      <div className={styles.allResults}>
+        {pokemonsList && (
+          pokemonsList.map((pokemon) => {
+            return (
+              <Card
+                key={pokemon?.id} 
+                sprite={pokemon?.sprites?.other?.dream_world?.front_default 
+                  ? pokemon.sprites?.other?.dream_world?.front_default 
+                  : pokemon.sprites?.front_default} 
+                name={pokemon?.name} 
+                pokeId={pokemon?.id}
+                stats={pokemon?.stats} 
+                isLoading={isLoading}
+              />
+            )
+          })
+        )}
+      </div>
     </Layout>
   )
 }
